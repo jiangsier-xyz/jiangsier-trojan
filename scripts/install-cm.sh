10da0dc55c28c685f692c252ab06aae135052db6
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2024-10-23 17:30:11 +0800

    chore: adjust scripts for installing cm & in

--- scripts/install-cm.sh
+++ scripts/install-cm.sh
@@ -5,11 +5,12 @@ source $(dirname ${BASH_SOURCE[0]})/setenv.sh
 check_helm
 
 helm repo add jetstack https://charts.jetstack.io &>/dev/null
+helm repo update
 helm --kubeconfig ${KUBE_CONFIG} install --create-namespace -f ${values_yaml} \
-  cert-manager jetstack/cert-manager \
   --namespace ${HELM_cert_manager_namespace} \
   --version ${HELM_cert_manager_version} \
-  --set installCRDs=true
+  --set installCRDs=true \
+  cert-manager jetstack/cert-manager
 
 helm --kubeconfig ${KUBE_CONFIG} install -f ${values_yaml} \
   --set trojan.enabled=false \
