ad6f397274a25f2e07fccad0a4cc7c8aa4bffbd1
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2024-10-28 11:10:37 +0800

    refactor: cert-manager

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,5 @@ source $(dirname ${BASH_SOURCE[0]})/setenv.sh
 check_helm
 
 helm install --kubeconfig ${KUBE_CONFIG} --namespace ${NAMESPACE} --create-namespace -f ${values_yaml} \
-  --set cert.clusterIssuer.enabled=false \
   ${ARGS[*]} \
   ${PROJECT_NAME} ${HELM_CONFIG_HOME}
